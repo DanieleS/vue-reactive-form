1a0a953553332c46feb31863656dbe30a6f70ff7
Tommaso Gangemi <gangemi.tommaso@gmail.com>
2025-09-11 09:04:13 +0200

    chore(createControlsTree): move util to index file as it's never used as standalone

--- src/new-interface-poc/index.ts
+++ src/new-interface-poc/index.ts
@@ -1,8 +1,76 @@
 import { ref, type Ref } from "@vue/reactivity"
 import type { Object } from "ts-toolbelt"
-import { cloneDeep } from "lodash-es"
-import type { FormControl, InputControl } from "./types"
-import { createControlsTree } from "./controlsTree"
+import { cloneDeep, get } from "lodash-es"
+import type { FormControl, FormNode, InputControl } from "./types"
+import { getInputControl } from "./inputControl"
+
+const createControlsTree = <TState>(
+  formState: Ref<TState>,
+  defaultFormState: Ref<TState>,
+  controlsCache: Map<string, InputControl<unknown>>
+) => {
+  const buildProxyHandler = (path: (string | number | symbol)[] = []) => ({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    get(target: any, handlerPath: string | number | symbol) {
+      const fullPath = [...path, handlerPath]
+
+      if (handlerPath === Symbol.iterator) {
+        return function* () {
+          const array = get(formState.value, path) ?? []
+          for (let i = 0; i < array.length; i++) {
+            const iteratorPath = [...path, i]
+            target[i] = buildProxyControl(
+              formState,
+              defaultFormState,
+              controlsCache,
+              iteratorPath
+            )
+            yield target[i]
+          }
+        }
+      }
+
+      if (!Reflect.get(target, handlerPath)) {
+        Reflect.set(
+          target,
+          handlerPath,
+          buildProxyControl(
+            formState,
+            defaultFormState,
+            controlsCache,
+            fullPath
+          )
+        )
+      }
+
+      return Reflect.get(target, handlerPath)
+    }
+  })
+
+  const buildProxyControl = (
+    formState: Ref<TState>,
+    defaultFormState: Ref<TState>,
+    controlsCache: Map<string, InputControl<unknown>>,
+    path: (string | number | symbol)[]
+  ) => {
+    return new Proxy(
+      {
+        control: getInputControl(
+          formState,
+          defaultFormState,
+          controlsCache,
+          path
+        )
+      },
+      buildProxyHandler(path)
+    )
+  }
+
+  return new Proxy<FormNode<TState>>(
+    {} as FormNode<TState>,
+    buildProxyHandler()
+  )
+}
 
 type FormState<T> = T extends object ? Object.Partial<T, "deep"> : T
 
