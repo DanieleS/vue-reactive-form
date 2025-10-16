4261f83eae890d3af716c2f20ca0ca58ac975ccf
Tommaso Gangemi <gangemi.tommaso@gmail.com>
2025-10-16 16:33:15 +0200

    feat(deep-partial-state): turn control and form state to deep partial

--- src/types/controls.ts
+++ src/types/controls.ts
@@ -2,14 +2,14 @@ import type { ComputedRef, Ref } from "@vue/reactivity"
 import type { PartialOrPrimitive } from "./utils"
 
 export type InputControl<T> = {
-  state: Ref<T | undefined>
-  defaultValue: ComputedRef<T | undefined>
+  state: Ref<PartialOrPrimitive<T> | undefined>
+  defaultValue: ComputedRef<PartialOrPrimitive<T> | undefined>
   dirty: ComputedRef<boolean>
   isValid: ComputedRef<boolean>
   errorMessage: ComputedRef<string | undefined>
   clear: () => void
   reset: () => void
-  updateDefaultValue: (newDefaultValue?: T) => void
+  updateDefaultValue: (newDefaultValue?: PartialOrPrimitive<T>) => void
 }
 
 export type ArrayInputControl<T extends Array<unknown>> = InputControl<T> & {
